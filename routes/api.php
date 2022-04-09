780d9cec993e026904cd029c23c10ca13c3b520b
“csl616515” <1300216200@qq.com>
2022-04-09 19:03:38 +0800

    csl第1次

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,17 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+/**
+ * csl
+ */
+Route::prefix('province')->group(function () {
+    Route::get('show','ProvinceController@Show');//节目审批展示
+    Route::post('showci','ProvinceController@Showci');//查看详词作者
+    Route::post('showxiangmu','ProvinceController@Showxiangmu');//查看详情项目负责人
+    Route::post('showqu','ProvinceController@Showqu');//查看详情曲作者
+    Route::post('showall','ProvinceController@Showall');//查看详情
+    Route::post('reject','ProvinceController@Reject');//一键驳回
+    Route::post('type','ProvinceController@Type');//活动类型
+    Route::post('inquire','ProvinceController@Inquire');//根据学校名查询
+    Route::get('look','ProvinceController@Look');//审批记录
+});
